c57d238b1d1194f85973bf2e18f16d5c85185f69
AndreyRasputin2002 <a.rasputin228@gmail.com>
2023-11-23 14:19:34 +0300

    6 add bd in second tests

--- andrey-rasputin-kt-42-20.Test/PepodDegreeIntegrationTests.cs
+++ andrey-rasputin-kt-42-20.Test/PepodDegreeIntegrationTests.cs
@@ -26,13 +26,81 @@ namespace andrey_rasputin_kt_42_20.Test
         public async Task GetPrepodsByDegreeAsync_KT4220_TwoObjects()
         {
             // Arrange
+            
             var ctx = new PrepodDbContext(_dbContextOptions);
             var degreeService = new DegreeService(ctx);
-   
-
+            var prepodService = new PrepodService(ctx);
+            var kafedra = new List<Kafedra>
+            {
+                new Kafedra
+                {
+                    KafedraId =1,
+                    KafedraName = "кафедра компьютерных технологий"
+                },
+                new Kafedra
+                {
+                    KafedraId =2,
+                    KafedraName = "кафедра инженерной техники"
+                }
+            };
+            await ctx.Set<Kafedra>().AddRangeAsync(kafedra);
 
             await ctx.SaveChangesAsync();
 
+            var degree = new List<Degree>
+            {
+                new Degree
+                {
+                    DegreeId =1,
+                    Name_degree = "доктор наук"
+                },
+                new Degree
+                {
+                    DegreeId =2,
+                    Name_degree = "кандидат наук"
+                }
+            };
+
+            await ctx.Set<Degree>().AddRangeAsync(degree);
+
+            await ctx.SaveChangesAsync();
+
+            var prepods = new List<Prepod>
+            {
+                new Prepod
+                {
+                    FirstName = "123",
+                    LastName = "123",
+                    MiddleName = "123",
+                    Telephone = "555",
+                    Mail = "123@mail.ru",
+                    KafedraId = 1,
+                    DegreeId = 2
+                },
+                new Prepod
+                {
+                    FirstName = "mem",
+                    LastName = "mem",
+                    MiddleName = "mem",
+                    Telephone = "9999",
+                    Mail = "mem@gmail.com",
+                    KafedraId = 1,
+                    DegreeId = 1
+                },
+                new Prepod
+                {
+                    FirstName = "mem1",
+                    LastName = "mem1",
+                    MiddleName = "mem1",
+                    Telephone = "99991",
+                    Mail = "mem1@gmail.com",
+                    KafedraId = 2,
+                    DegreeId = 1
+                }
+            };
+            await ctx.Set<Prepod>().AddRangeAsync(prepods);
+
+            await ctx.SaveChangesAsync();
             // Act
             var filter = new PrepodDegreeFilter
             {
